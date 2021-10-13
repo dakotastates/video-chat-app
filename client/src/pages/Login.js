f8a7a1d7fae2bee106d8a35c6f0eb834b6b9fcda
Dakota States <dakota.states@gmail.com>
2021-10-13 14:34:36 -0700

    Register and Login bootstrap styling

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -19,28 +19,42 @@ const Login = ({ setAuth }) => {
   };
 
   return (
-    <div>
+    <div className="d-flex flex-column container ">
       <h1>Login</h1>
       <form onSubmit={onSubmitForm}>
-        <input
-          type="text"
-          name="email"
-          placeholder="Email"
-          value={email}
-          onChange={e => onChange(e)}
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={password}
-          onChange={e => onChange(e)}
-        />
-        <button class="btn btn-success btn-block">Login</button>
+        <div className="form-group">
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={email}
+            onChange={e => onChange(e)}
+            className="form-control input"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={password}
+            onChange={e => onChange(e)}
+            className="form-control input"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary btn-sm submit">Sign In</button>
       </form>
-      <Link to="/register">register</Link>
+
+      <h7>Need an account?</h7>
+      <Link className="btn btn-secondary btn-sm signup-btn" to="/register">Register</Link>
+      <Link className="btn btn-secondary btn-sm back-btn " to="/">Back</Link>
     </div>
   );
 };
 
 export default Login;
+
+// <label for="exampleInputEmail1">Email address</label>
+// <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+// <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -6,10 +6,11 @@ const Register = () =>{
   const [inputs, setInputs] = useState({
     email: "",
     password: "",
+    confirmPassword: "",
     name: ""
   })
 
-  const {email, password, name} = inputs
+  const {email, password, confirmPassword, name} = inputs
 
   const onChange = (e) =>{
     setInputs({...inputs, [e.target.name]: e.target.value})
@@ -21,33 +22,57 @@ const Register = () =>{
   }
 
   return (
-    <div>
-      <h1>Register</h1>
+    <div className="d-flex flex-column container ">
+
       <form onSubmit={onSubmitForm}>
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={email}
-          onChange={e => onChange(e)}
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={password}
-          onChange={e => onChange(e)}
-        />
-        <input
-          type="text"
-          name="name"
-          placeholder="Name"
-          value={name}
-          onChange={e => onChange(e)}
-        />
-        <button>Register</button>
+        <h1>Sign Up</h1>
+
+        <div className="form-group">
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={name}
+            onChange={e => onChange(e)}
+            className="form-control input"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={email}
+            onChange={e => onChange(e)}
+            className="form-control input"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={password}
+            onChange={e => onChange(e)}
+            className="form-control input"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            name="confirmPassword"
+            placeholder="Confirm Password"
+            value={confirmPassword}
+            onChange={e => onChange(e)}
+            className="form-control input"
+          />
+        </div>
+
+        <button className="btn btn-primary btn-sm submit">Sign Up</button>
       </form>
-      <Link to="/login">login</Link>
+        <h7>Already have an account?</h7>
+        <Link className="btn btn-secondary btn-sm signup-btn" to="/login">Login</Link>
+        <Link className="btn btn-secondary btn-sm back-btn " to="/">Back</Link>
     </div>
   )
 }
